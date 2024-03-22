d5014cb8bbe10c87fe75cd37e6869b8a2f9fb58f
aaronbecher <Aaronbecher@gmx.de>
2024-03-22 15:39:55 +0100

    Init spring boot backend

---
+++ backend/src/test/java/de/quarzerback/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.quarzerback.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
