fb1dfc28a135e9dfd6e5f15e36915a128678b0db
aaronbecher <Aaronbecher@gmx.de>
2024-03-28 16:42:03 +0100

    Changed Service test to working unit test

--- backend/src/test/java/de/quarzerback/backend/service/TrainingServiceTest.java
+++ backend/src/test/java/de/quarzerback/backend/service/TrainingServiceTest.java
@@ -1,43 +1,26 @@
 package de.quarzerback.backend.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
 import de.quarzerback.backend.model.Training;
 import de.quarzerback.backend.model.dtos.TrainingDto;
 import de.quarzerback.backend.repository.TrainingRepo;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import lombok.RequiredArgsConstructor;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 
 public class TrainingServiceTest {
 
-    private final TrainingRepo trainingRepo = mock(TrainingRepo.class);
-
-    private final TrainingService trainingService = new TrainingService(trainingRepo);
-
     @Test
-    public void testAddTraining() {
-        // Given
+    void testAddTraining(){
         TrainingDto trainingDto = new TrainingDto();
-        trainingDto.setDate(LocalDate.now());
-        trainingDto.setStartTime(LocalTime.now());
-        trainingDto.setEndTime(LocalTime.now().plusHours(1));
-        trainingDto.setSpecificCoachingPoints("Focus on passing drills");
-        trainingDto.setNeededMaterial("Cones, balls");
+        TrainingRepo trainingRepo = mock(TrainingRepo.class);
 
-        // When
-        Training addedTraining = trainingService.addTraining(trainingDto);
+        when(trainingRepo.save(any())).thenReturn(new Training());
 
-        // Then
-        assertNotNull(addedTraining.getId());
-        assertEquals(trainingDto.getDate(), addedTraining.getDate());
-        assertEquals(trainingDto.getStartTime(), addedTraining.getStartTime());
-        assertEquals(trainingDto.getEndTime(), addedTraining.getEndTime());
-        assertEquals(trainingDto.getSpecificCoachingPoints(), addedTraining.getSpecificCoachingPoints());
-        assertEquals(trainingDto.getNeededMaterial(), addedTraining.getNeededMaterial());
+        TrainingService trainingService = new TrainingService(trainingRepo);
+
+        Training result = trainingService.addTraining(trainingDto);
+
+        assertNotNull(result);
     }
 }
